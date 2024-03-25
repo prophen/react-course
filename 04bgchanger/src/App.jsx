c3ec7c65476c84f7d6c138a421756b938e1011d2
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2024-03-24 18:18:48 -0700

    05passwordgenerator

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -18,28 +18,28 @@ function App() {
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
             onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "red" }}
           >
             red
           </button>
           <button
             onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "green" }}
           >
             green
           </button>
           <button
             onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "blue" }}
           >
             blue
           </button>
           <button
             onClick={() => setColor("violet")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "violet" }}
           >
             violet
