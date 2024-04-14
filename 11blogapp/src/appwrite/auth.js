f1429a53f45f07378c11d4624405e9285fdcedd9
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2024-04-14 14:15:23 -0700

    update auth.js

--- 11blogapp/src/appwrite/auth.js
+++ 11blogapp/src/appwrite/auth.js
@@ -1,8 +1,11 @@
-import conf from "../conf/conf";
+// start with appwrite auth service
+import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
   client = new Client();
+  account;
+
   constructor() {
     this.client
       .setEndpoint(conf.appwriteUrl)
@@ -29,7 +32,7 @@ export class AuthService {
   }
   async login({ email, password }) {
     try {
-      return await this.account.createEmailPasswordSession(email, password);
+      return await this.account.createEmailSession(email, password);
     } catch (error) {
       throw error;
     }
